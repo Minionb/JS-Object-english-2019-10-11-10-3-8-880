1bafc68dfec1b7dab51ff12040dcfcaf1d5d2f0d
Minionb <43756995+Minionb@users.noreply.github.com>
2019-11-14 19:06:54 +0800

    The first commit

---
+++ 2.js
@@ -0,0 +1,8 @@
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+    };
+
+var fruitNumber = fruit.apple + fruit.pear + fruit.peach; //add the number of fruit
+console.log(fruitNumber);  //Output = 50
